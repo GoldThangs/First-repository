d5ae06d0cb5a1d1231158914c1815351efd83b20
Tselikov Bogdan <Faceit_1308@mail.ru>
2020-05-14 15:40:15 +0500

    third commit

--- Summ.pas
+++ Summ.pas
@@ -1,4 +1,4 @@
-﻿var a,b,c,d:integer;
+﻿var a,b,c,d,e:integer;
 begin
 Writeln('Введите первое число');
 read(a);
@@ -8,4 +8,6 @@ c:=a+b;
 writeln('Сумма = ',c);
 d:=a*b;
 writeln('Произведение = ',d);
+e:=(a*b)-(a+b);
+writeln('Разность произведения и суммы 2-х чисел = ',e);
 end.
